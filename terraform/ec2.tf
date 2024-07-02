660da59eda14c43b9dccdd082bbec10d9ad308f1
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-07-02 14:58:08 +0530

    update ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,15 +11,56 @@ resource "aws_instance" "strapi" {
 
   user_data = <<-EOF
               #!/bin/bash
+              # Update the package index
               sudo apt update -y
+
+              # Install Node.js, npm, and git
               sudo apt install -y nodejs npm git
-              npm install -g strapi@latest
+
+              # Install PM2 globally
+              sudo npm install -g pm2
+
+              # Install Strapi globally
+              sudo npm install -g strapi@latest
+
+              # Create directory for Strapi
               mkdir -p /var/www/strapi
+
+              # Navigate to the directory
               cd /var/www/strapi
+
+              # Clone the repository
               git clone https://github.com/arkajyotiadhikary/ContentEcho.git
+
+              # Navigate to the repository
               cd ContentEcho
+
+              # Checkout the specific branch
               git checkout strapi-ec2
+
+              # Install dependencies
               npm install
-              npm run develop
               EOF
+}
+
+resource "null_resource" "provision" {
+  depends_on = [aws_instance.strapi]
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.strapi.public_ip
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Running remote-exec provisioner'",
+      "cd /var/www/strapi/ContentEcho",
+      "pm2 start npm --name 'strapi' -- run develop",
+      "pm2 save",
+      "pm2 startup systemd -u $(whoami) --hp /home/$(whoami)",
+      "sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u $(whoami) --hp /home/$(whoami)"
+    ]
+  }
 }
