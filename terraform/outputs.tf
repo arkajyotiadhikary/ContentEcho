e2cbae2184241c23f5aff34f099372a08bf46d8c
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-07-03 22:15:26 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,7 @@
 output "instance_id" {
-  value = aws_instance.ar-strapi[0].id
+  value = aws_instance.ar-strapi.id
 }
 
 output "public_ip" {
-  value = aws_instance.ar-strapi[0].public_ip
+  value = aws_instance.ar-strapi.public_ip
 }
-
