dc91ba41dd04b7b45e822419b69a2f8fc64716f8
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-07-04 20:18:38 +0530

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -10,7 +10,6 @@ resource "aws_route53_record" "subdomain_record" {
 # Data source to fetch Route 53 hosted zone ID
 data "aws_route53_zone" "main" {
   id = "Z06607023RJWXGXD2ZL6M"
-  name = var.domain_name
 }
 
 # Output the subdomain DNS name
